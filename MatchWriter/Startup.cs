caf4a3c9af97954cf6f89023e196f1c1b02e1bf9
Kieron <kieron@mentor.gg>
2020-03-31 10:58:43 +0200

    Enable Automigration on Startup (moved from MatchRetriever due to db permissions)

--- MatchWriter/Startup.cs
+++ MatchWriter/Startup.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -156,7 +157,7 @@ namespace MatchWriter
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
         {
             if (env.IsDevelopment())
             {
@@ -181,8 +182,14 @@ namespace MatchWriter
             {
                 options.RoutePrefix = "swagger";
                 options.SwaggerEndpoint("/swagger/v1/swagger.json", "MatchRetriever");
-            });            
+            });
             #endregion
+
+            // migrate if this is not an inmemory database
+            if (services.GetRequiredService<MatchContext>().Database.ProviderName != "Microsoft.EntityFrameworkCore.InMemory")
+            {
+                services.GetRequiredService<MatchContext>().Database.Migrate();
+            }
         }
 
         /// <summary>
